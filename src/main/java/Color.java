fbc12d9bb4774958b04254c54d795780cfce70f7
zalex <mabutamail@gmail.com>
2019-09-30 17:44:28 +0300

    ddd

--- src/main/java/Color.java
+++ src/main/java/Color.java
@@ -1,23 +1,18 @@
 enum Color {        //неявно - public static
-    COLOR4_0("4+0"),
-    COLOR4_4("4+4"),
-    COLOR1_0("1+0"),
-    COLOR1_1("1+1"),
-    COLOR4_1("4+1");
+    COLOR4_0("4+0") { public double apply(double x, double y) { return x + y; } },
+    COLOR4_4("4+4") {},
+    COLOR1_0("1+0") {},
+    COLOR1_1("1+1") {},
+    COLOR4_1("4+1") {};
 
-    private String color;
+    private final String color;
 
     //конструктор
-    Color(String color) {
-        this.color = color;
-    }
+    Color(String color) { this.color = color; }
+
     //геттер
-    String getColor(){
-        return color;
-    }
+    public String getColor(){ return color; }
 
     @Override
-    public String toString() {
-        return color;
-    }
+    public String toString() { return color; }
 }
