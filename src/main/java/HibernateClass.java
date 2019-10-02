530c9c77cdbdf56239668b3814340f75b82b0a39
mabutamail <46491924+mabutamail@users.noreply.github.com>
2019-10-02 12:58:35 +0300

    Update HibernateClass.java

--- src/main/java/HibernateClass.java
+++ src/main/java/HibernateClass.java
@@ -1,17 +1,7 @@
 public class HibernateClass {
 
 
-    //DB        dragon660_javadb
-    //login     032670002_javadb
-    //password  javadb
 
-
-//    Адрес MySQL-сервера	localhost
-//    Порт	3306
-//    Локальный IP	127.0.0.1
-//    Адрес для внешних подключений	mysql.dragon660.myjino.ru
-//    Сокет	/var/lib/mysql/mysql.sock
-//    Имя пользователя	dragon660
 
 
 }
