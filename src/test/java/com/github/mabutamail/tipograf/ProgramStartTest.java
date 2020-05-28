c24fdffab6cb183fd1f080d80d37d98f17a96d9d
zalex <mabutamail@gmail.com>
2020-05-28 18:10:39 +0300

    sprint 1 JPA

--- src/test/java/com/github/mabutamail/tipograf/ProgramStartTest.java
+++ src/test/java/com/github/mabutamail/tipograf/ProgramStartTest.java
@@ -8,14 +8,64 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
 public class ProgramStartTest {
     public final static Logger logger = LoggerFactory.getLogger(com.github.mabutamail.tipograf.ProgramStart.class);
-//    public EntityManager em = Persistence.createEntityManagerFactory("TIPOGRAF").createEntityManager();
 
     @Test
     public void main() {
         logger.info("====================       Начало программы        ====================");
+//        sessionExample();
+        jpaExample();
 
+        logger.info("====================       Конец программы         ====================");
+
+//        logger.error("ERROR - информация об ошибках, возникших в работе модулей");
+//        logger.warn("WARNING - информация о событиях, которые могут привести к ошибкам в работе модулей");
+//        logger.info("INFO - подтверждение, информация о событиях, не приводящих к ошибкам в работе модулей");
+//        logger.debug("DEBUG - подробное и детальное логирование всей системной информации");
+//        logger.trace("TRACE: менее приоритетные логи для отладки, с наименьшим уровнем логирования");
+
+    }
+
+    private void jpaExample() {
+        EntityManager em = Persistence.createEntityManagerFactory("TIPOGRAF").createEntityManager();
+        logger.info("====================       создан Entity Manager         ====================");
+
+        em.getTransaction().begin();
+        logger.info("====================       начало транзакции         ====================");
+
+        Client clientEM = new Client();
+        clientEM.setClientName("Рога и Копыта _ Entity Manager");
+        clientEM.setClientComment("тел (495) 123-45-67 Entity Manager");
+        em.persist(clientEM);
+        logger.info("====================       Клиент создан Entity Manager        ====================");
+
+        PrintOrder zakazEM = new PrintOrder();
+        zakazEM.setClient(clientEM);
+        zakazEM.setPrice(222_456L);
+        zakazEM.setComment("оплата наличными, готовность - к 31.05.20 17:00 Entity Manager");
+        zakazEM.setClient(clientEM);
+        clientEM.getPrintOrders().add(zakazEM);
+        em.persist(zakazEM);
+        logger.info("====================       Заказ создан Entity Manager         ====================");
+
+        em.getTransaction().commit();
+        logger.info("====================       Транзакция завершена Entity Manager         ====================");
+
+        logger.info("====================       Список клиентов1 Entity Manager         ====================");
+        logger.info("{}\n", em.createQuery("from Client").getResultList());
+        logger.info("====================       Список заказов Entity Manager         ====================");
+        logger.info("{}\n", em.createQuery("from PrintOrder").getResultList());
+
+        em.close();
+        logger.info("====================       Entity Manager закрыт         ====================");
+    }
+
+
+    private void sessionExample() {
         Session session = HibernateSessionFactory.getSessionFactory().openSession();
         logger.info("====================       получили коннект к БД         ====================");
 
@@ -54,13 +104,5 @@ public class ProgramStartTest {
 
         session.close();
         logger.info("====================       Сессия закрыта         ====================");
-        logger.info("====================       Конец программы         ====================");
-
-//        logger.error("ERROR - информация об ошибках, возникших в работе модулей");
-//        logger.warn("WARNING - информация о событиях, которые могут привести к ошибкам в работе модулей");
-//        logger.info("INFO - подтверждение, информация о событиях, не приводящих к ошибкам в работе модулей");
-//        logger.debug("DEBUG - подробное и детальное логирование всей системной информации");
-//        logger.trace("TRACE: менее приоритетные логи для отладки, с наименьшим уровнем логирования");
-
     }
 }
