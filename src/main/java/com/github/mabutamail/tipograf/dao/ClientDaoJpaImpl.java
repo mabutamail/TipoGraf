3edc0b6aa05dda0dccc4f3db5429db0be85ff011
zalex <mabutamail@gmail.com>
2020-06-02 17:15:34 +0300

    C3P0 connection pool in Hibernate

---
+++ src/main/java/com/github/mabutamail/tipograf/dao/ClientDaoJpaImpl.java
@@ -0,0 +1,26 @@
+package com.github.mabutamail.tipograf.dao;
+
+import java.io.Serializable;
+
+public class ClientDaoJpaImpl implements Dao {
+
+    @Override
+    public Serializable create(Object newInstance) {
+        return null;
+    }
+
+    @Override
+    public Object read(Serializable id) {
+        return null;
+    }
+
+    @Override
+    public void update(Object transientObject) {
+
+    }
+
+    @Override
+    public void delete(Object persistentObject) {
+
+    }
+}
